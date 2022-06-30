15d8e3d69963b0c59117e65294e0bc15d778d2ca
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-30 18:38:35 +0500

    Update all animals' weights

--- queries.sql
+++ queries.sql
@@ -25,3 +25,12 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > DATE '2022-01-01';
+SAVEPOINT SP01;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP01;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
