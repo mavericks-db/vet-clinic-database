47e3cd7c1676aa8e434328d93d2a3a7ee7b2c934
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-30 18:29:33 +0500

    Update the animals table by setting the species column to unspecified

--- queries.sql
+++ queries.sql
@@ -8,3 +8,9 @@ SELECT name, escape_attempts, weight_kg FROM animals WHERE weight_kg > 10.5;
 SELECT name, neutered FROM animals WHERE neutered = 'TRUE';
 SELECT name FROM animals WHERE NOT name = 'Gabumon';
 SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
