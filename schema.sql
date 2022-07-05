54a5c001be8fda7917205f347c35a4a00014acbe
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-07-05 22:17:54 +0500

    Modify animals table

--- schema.sql
+++ schema.sql
@@ -22,3 +22,21 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
