f459888d70ee13ed35aadea83ed8e2daacbd2188
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-07-06 14:20:56 +0500

    Create a join table called visits

--- schema.sql
+++ schema.sql
@@ -55,3 +55,9 @@ CREATE TABLE specializations (
     vet_id INT REFERENCES vets (id),
     species_id INT REFERENCES species (id)
 );
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals (id),
+    vet_id INT REFERENCES vets (id),
+    date_of_visit DATE
+);
