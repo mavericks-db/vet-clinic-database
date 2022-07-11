0726b9ff83eb06d3ef2c220c656a0a9c1c1b14b9
Baroka <birotori@gmail.com>
2022-07-11 14:53:29 +0100

    Improve execution time of the other two queries

--- schema.sql
+++ schema.sql
@@ -64,3 +64,7 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners
 ADD COLUMN email VARCHAR(120);
+-- CREATE INDEX TO OPTIMAZE QUERY ---
+CREATE INDEX visits_animal_id_idx ON visits(animals_id);
+CREATE INDEX visits_vet_id_idx ON visits(vets_id);
+CREATE INDEX owners_email_idx ON owners(email);
