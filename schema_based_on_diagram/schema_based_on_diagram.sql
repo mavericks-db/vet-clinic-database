ed21310c76ffe6ba6b22c57e6002c0b149b0e12e
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-07-11 19:57:31 +0500

    Create a database based on a diagram

---
+++ schema_based_on_diagram/schema_based_on_diagram.sql
@@ -0,0 +1,39 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE DATABASE clinic;
+
+CREATE TABLE patients (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE
+);
+
+CREATE TABLE medical_histories(
+    id INT PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(255)
+);
+
+CREATE TABLE treatments(
+    id INT PRIMARY KEY,
+    type VARCHAR(255),
+    name VARCHAR(255)
+);
+
+CREATE TABLE invoices(
+    id INT PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items(
+    id INT,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id)
+);
